3bae69bab3440d1b4332b6679d847e9c82efb839
Henning Håkonsen <henninghaakonsen@me.com>
2020-06-02 14:15:25 +0200

    Alltid returner 4820 enhet.

--- src/norg2/norg2.ts
+++ src/norg2/norg2.ts
@@ -2,8 +2,8 @@ import { Express, Request, Response } from 'express';
 import { lesMockFil } from '../common';
 
 export default (app: Express) => {
-    const hentEnhet = (enhet?: string) => {
-        return lesMockFil(`enhet_${enhet ? enhet : '4820'}.json`);
+    const hentEnhet = (_enhet?: string) => {
+        return lesMockFil(`enhet_4820.json`);
     };
 
     app.get('/rest/api/norg2/api/v1/enhet/:enhet', (req: Request, res: Response) => {
