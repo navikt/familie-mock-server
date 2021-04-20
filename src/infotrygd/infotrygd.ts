e417701c180fcd935be170371b924cb1ab3ec4b4
Geirmund Simonsen <geirmund.simonsen@nav.no>
2021-04-20 11:45:05 +0200

    lag infotrygd mock

--- src/infotrygd/infotrygd.ts
+++ src/infotrygd/infotrygd.ts
@@ -1,1 +1,10 @@
-// foo
+import { Express, Request, Response } from 'express';
+
+export default (app: Express) => {
+    app.post(
+        '/rest/api/infotrygd/ba/infotrygd/barnetrygd/saker',
+        (_req: Request, res: Response) => {
+            res.json({ barn: [], bruker: [] });
+        },
+    );
+};
